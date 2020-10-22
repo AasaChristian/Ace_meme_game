bb5fe6427152220251f685583f1aa13b6958cbde
Aasa-Christian <aasachristian@yahoo.com>
2020-10-21 22:06:17 -0400

    updated login component

--- ace_meme_game/src/components/login.js
+++ ace_meme_game/src/components/login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import Axios from 'axios'
 
 
 const Login = (props) =>{
@@ -9,6 +10,7 @@ const [credentials, setCredentials] = useState({
     password: '',
 });
 const [room, setRoom] = useState('')
+
 console.log(room, "room")
 
 const handleChange = e => {
@@ -16,27 +18,38 @@ const handleChange = e => {
 }
 
 const handleChangeRoom = e => {
-    setRoom({...room, [e.target.name]: e.target.value})
+    setRoom(e.target.value)
 }
 const login = e => {
     e.preventDefault()
-    console.log(props.socket, "props socket")
-    props.socket.emit('login', credentials)
+    console.log( "Help Me!")
+    // props.socket.emit('login', credentials)
     // console.log(log, "log")
-    props.history.push('/home')
+    Axios
+    .post('http://localhost:5000/api/login', credentials)
+    .then(res => {
+        // localStorage.setItem('token', res.data.payload);
+        console.log(res, "Token")
+    })
+    .catch(err => console.log(err))
+
+    // props.history.push('/home')
 }
 
 props.socket.on('token', load => {
     console.log(load)
-    localStorage.setItem('token', load.Token)
+    // localStorage.setItem('token', load.Token)
     localStorage.setItem('username', load.username)
 })
 return(
    <form onSubmit={login} className="loginForm">
-       <input type="text" name="room" onChange={handleChangeRoom} placeholder="Room" />
+        <input type="text" name="room" onChange={handleChangeRoom} placeholder="Room" />
+    
         <input type="text" name="username" onChange={handleChange} placeholder="UserName" />
         <input type="text" name="password" onChange={handleChange} placeholder="PassWord"/>
         <button type="submit" className="loginBTN">LOGIN</button>
+        {/* <Link onClick={e => (!credentials.username  || !room) ? e.preventDefault(): null} to={`/home?name=${credentials.username}&room=${room}` && {login}}>
+        </Link> */}
 </form> 
 
 )
