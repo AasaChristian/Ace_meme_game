6ce29945effe5161bfc0630acc6c03f032aee27b
Aasa-Christian <aasachristian@yahoo.com>
2020-10-27 19:28:26 -0400

    added input and functionality to search all rooms by name

--- ace_meme_game/src/components/joinRoom.js
+++ ace_meme_game/src/components/joinRoom.js
@@ -6,9 +6,11 @@ const JoinRoom = (props) =>{
     const userName = localStorage.getItem('username')
     const [room, setRoom] = useState('')
     const [existingRoom, setExistingRoom] = useState([])
+    const [searchRoom, setSearchRoom] = useState([])
+    console.log(searchRoom, "searched room")
 // sends server user name to have connected rooms returned
     useEffect(() => {
-        props.socket.emit("selectRoom", userName)
+        props.socket.emit("sendUserNameForConnectedRooms", userName)
     },[])
 // socket from server, retunrs all rooms user is part of.
 props.socket.on("preRoom", preRooms => {
@@ -17,6 +19,10 @@ props.socket.on("preRoom", preRooms => {
 // handle change for new room inpute form
 const handleChangeRoom = e => {
     setRoom(e.target.value)
+}
+
+const handleSearchRoom = e => {
+    setSearchRoom(e.target.value)
 }
 // click even to create a new room
 const create = (e) => {
@@ -32,12 +38,21 @@ return(
         <input type="text" name="room" onChange={handleChangeRoom} placeholder="Room" />
         <button type="submit" className="loginBTN">Create Room</button>
 </form> 
+
+<form onSubmit={nun => {
+               props.socket.emit("JoinConnectedRoomsSelectedByClick", (({roomName: searchRoom, userName: userName})))
+               props.history.push('/home')
+            
+            } } className="loginForm">
+        <input type="text" name="searchRoom" onChange={handleSearchRoom} placeholder="Find Existing Room" />
+        <button type="submit" className="loginBTN">Find Room</button>
+</form> 
 <div>
     {existingRoom.map(rooms => {
         return(
             <div key ={rooms.id}>
            <button onClick={nun => {
-               props.socket.emit("selectedRoomName", (rooms.name))
+               props.socket.emit("JoinConnectedRoomsSelectedByClick", ({roomName: rooms.name, userName: userName}))
                props.history.push('/home')
             
             } }
