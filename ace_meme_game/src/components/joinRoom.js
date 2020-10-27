c4427460ef1ef2d14163ffd20229c4df7b2b9859
Aasa-Christian <aasachristian@yahoo.com>
2020-10-27 14:23:23 -0400

    created joinin room component

--- ace_meme_game/src/components/joinRoom.js
+++ ace_meme_game/src/components/joinRoom.js
@@ -1,32 +1,59 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 
-const Login = (props) =>{
+const JoinRoom = (props) =>{
+    const userName = localStorage.getItem('username')
+    useEffect(() => {
+        props.socket.emit("selectRoom", userName)
+    },[])
 
 const [room, setRoom] = useState('')
+const [existingRoom, setExistingRoom] = useState([])
 
+props.socket.on("preRoom", preRooms => {
+    setExistingRoom(preRooms)
+})
 console.log(room, "room")
 
 const handleChangeRoom = e => {
     setRoom(e.target.value)
 }
 
-const create = () => {
-    props.socket.emit('roomName', {room: room})
+const create = (e) => {
+    e.preventDefault()
+    props.socket.emit('roomName', {roomName: room, userName})
 }
 
 
 
+
+
 return(
+    <div>
    <form onSubmit={create} className="loginForm">
         <input type="text" name="room" onChange={handleChangeRoom} placeholder="Room" />
         <button type="submit" className="loginBTN">Create Room</button>
 </form> 
+<div>
+    {existingRoom.map(rooms => {
+        return(
+            <div key ={rooms.id}>
+           <button onClick={nun => {
+               props.socket.emit("selectedRoomName", (rooms.name))
+            
+            } }
+           ><p>{rooms.name}</p></button>
+           </div>
+        )
+    })}
+</div>
+    </div>
+
 
 )
 
 
 }
 
-export default Login;
+export default JoinRoom;
